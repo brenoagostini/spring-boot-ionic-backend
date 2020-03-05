4a3c131168ca5c7da60fb999fb7ddefc5d8b8ecf
Breno Agostini <breno.agostinho@hotmail.com>
2020-03-05 14:08:13 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/breno/udemy/services/CategoriaService.java
+++ src/main/java/com/breno/udemy/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.breno.udemy.domain.Categoria;
 import com.breno.udemy.repositories.CategoriaRepository;
+import com.breno.udemy.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
