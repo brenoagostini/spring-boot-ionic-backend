68725455732c9285137e9494d7be0acd6a7dd8fc
Breno Agostini <breno.agostinho@hotmail.com>
2020-03-10 16:57:33 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/breno/udemy/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.breno.udemy.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.breno.udemy.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
