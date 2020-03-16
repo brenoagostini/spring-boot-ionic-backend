e47f7f058d6355822c6b6bf2ab0fa695c17217ef
Breno Agostini <breno.agostinho@hotmail.com>
2020-03-16 16:38:26 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/breno/udemy/services/CategoriaService.java
+++ src/main/java/com/breno/udemy/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.breno.udemy.domain.Categoria;
+import com.breno.udemy.dto.CategoriaDTO;
 import com.breno.udemy.repositories.CategoriaRepository;
 import com.breno.udemy.services.exceptions.DataIntegrityException;
 import com.breno.udemy.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
